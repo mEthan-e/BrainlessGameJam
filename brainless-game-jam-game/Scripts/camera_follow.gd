dc5740da997b2edcbdac9d8a8d6c867048e80f0c
mEthan-e <eathomas1012@gmail.com>
2026-07-08 15:31:14 +0200

    Update camera_follow.gd

--- brainless-game-jam-game/Scripts/camera_follow.gd
+++ brainless-game-jam-game/Scripts/camera_follow.gd
@@ -1,14 +1,21 @@
 extends Camera2D
 
 var is_players: bool = true
+var controllable_players: bool = false
 
 func _process(delta: float) -> void:
 	is_players = false
+	controllable_players = false
 	for sibling in get_parent().get_children():
 		if (sibling.is_in_group("players")):
 			is_players = true
 			if(sibling.is_controlling):
+				controllable_players = true
 				position = sibling.position
 	if (!is_players):
 		await get_tree().create_timer(1.0).timeout
 		get_tree().reload_current_scene()
+	if (!controllable_players):
+		for sibling in get_parent().get_children():
+			if (sibling.is_in_group("players")):
+				sibling.is_controlling = true
